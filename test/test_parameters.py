46f90849cbc84559a9b2cbb09a791ec6e97858c0
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-03-07 15:48:34 +0100

    Adapt paths of test calls

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 
 def test_minimal_call():
     check_output(
-        [sys.executable, "~/repo/TikiToMWiki.py",
+        [sys.executable, "/home/circleci/repo/TikiToMWiki.py",
          "https://fb1-7.bs.ptb.de/tiki/",
          "./test/math.tar"])
 
