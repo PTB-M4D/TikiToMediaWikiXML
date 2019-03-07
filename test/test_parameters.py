d2f7ab926b8cfc6c9f93df308a8b6bd703d577bb
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-03-07 16:00:31 +0100

    Keep on debugging CI config

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 
 def test_minimal_call():
     check_output(
-        [sys.executable, "/home/circleci/repo/TikiToMWiki.py",
+        [sys.executable, "../TikiToMWiki.py",
          "https://fb1-7.bs.ptb.de/tiki/",
          "./test/math.tar"])
 
@@ -24,6 +24,6 @@ def test_stdout_call():
                '\'mustermann\']\r\nand file uploads on these pages: ' \
                'dict_keys([])\r\n'.encode()
     result = check_output(
-        [sys.executable, "~/repo/TikiToMediaWikiXML/TikiToMWiki.py", "-o", "-",
+        [sys.executable, "TikiToMWiki.py", "-o", "-",
          "https://fb1-7.bs.ptb.de/tiki/", "./test/math.tar"])
     assert result == expected
