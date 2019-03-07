69bbb71bdabc5762a396a8e7c4b70d9ba1e923f8
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-03-07 11:03:51 +0100

    Add 'shell=False' to subprocess call in tests

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -5,10 +5,10 @@ import sys
 def test_minimal_call():
     subprocess.call(
         [sys.executable, "TikiToMWiki.py", "https://fb1-7.bs.ptb.de/tiki/",
-         "./test/math.tar"])
+         "./test/math.tar"], shell=False)
 
 
 def test_stdout_call():
     subprocess.call(
         [sys.executable, "TikiToMWiki.py",  "-o", "-",
-         "https://fb1-7.bs.ptb.de/tiki/", "./test/math.tar"])
+         "https://fb1-7.bs.ptb.de/tiki/", "./test/math.tar"], shell=False)
