32b03863939c518475499940558db5f63b9712f8
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-05-20 17:03:37 +0200

    Introduce class fixtures for tests

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -4,6 +4,18 @@ from subprocess import check_output
 
 class TestMath:
 
+    @classmethod
+    def teardown_class(cls):
+        """
+            This method deletes all files created during the tests with
+            filenames './test/math/math_*.xml'.
+        """
+        from glob import glob
+        import os
+
+        for filename in glob("./test/math/math_*.xml"):
+            os.remove(filename)
+
     @staticmethod
     def test_minimal_call():
         check_output(
@@ -14,20 +26,20 @@ class TestMath:
     @staticmethod
     def test_stdout_call():
         expected = '<mediawiki xml:lang="en">\r\n<siteinfo>\r\n<base>' \
-                       'https://fb1-7.bs.ptb.de/tiki/</base>\r\n</siteinfo>' \
-                       '\r\n<page>\r\n<title>Math testpage</title>\r\n' \
-                       '<revision>\r\n<id>1</id>\r\n<timestamp>' \
-                       '2018-03-12T12:38:31Z</timestamp>\r\n<contributor>' \
-                       '<username>mustermann</username></contributor>\r\n' \
-                       '<text xml:space="preserve">\r\n__TOC__\r\n\r\n' \
-                       '{{mathjax}}\r\n\r\na(t) = \\frac{1}{2}\\' \
-                       'left[\r\n\r\na_1 sin(\\omega t) + a_2 sin(' \
-                       '\\omega t - \\varphi_0)\r\n\r\n\\right]\r\n\r\n{{' \
-                       '/mathjax}}\r\n\r\n</text>\r\n</revision>' \
-                       '\r\n</page>\r\n</mediawiki>\r\n\r\nnumber of pages ' \
-                       '= 1 number of versions = 1\r\nwith contributions by ' \
-                       '[\'mustermann\']\r\nand file uploads on these pages:' \
-                       ' dict_keys([])\r\n'
+                   'https://fb1-7.bs.ptb.de/tiki/</base>\r\n</siteinfo>' \
+                   '\r\n<page>\r\n<title>Math testpage</title>\r\n' \
+                   '<revision>\r\n<id>1</id>\r\n<timestamp>' \
+                   '2018-03-12T12:38:31Z</timestamp>\r\n<contributor>' \
+                   '<username>mustermann</username></contributor>\r\n' \
+                   '<text xml:space="preserve">\r\n__TOC__\r\n\r\n' \
+                   '{{mathjax}}\r\n\r\na(t) = \\frac{1}{2}\\' \
+                   'left[\r\n\r\na_1 sin(\\omega t) + a_2 sin(' \
+                   '\\omega t - \\varphi_0)\r\n\r\n\\right]\r\n\r\n{{' \
+                   '/mathjax}}\r\n\r\n</text>\r\n</revision>' \
+                   '\r\n</page>\r\n</mediawiki>\r\n\r\nnumber of pages ' \
+                   '= 1 number of versions = 1\r\nwith contributions by ' \
+                   '[\'mustermann\']\r\nand file uploads on these pages:' \
+                   ' dict_keys([])\r\n'
         expected_lin = expected.replace('\r\n', '\n').encode()
         expected_win = expected.encode()
         result = check_output(
@@ -37,6 +49,18 @@ class TestMath:
 
 
 class TestImages:
+
+    @classmethod
+    def teardown_class(cls):
+        """
+            This method deletes all files created during the tests with
+            filenames './test/images/Image testpage_*.xml'.
+        """
+        from glob import glob
+        import os
+
+        for filename in glob("./test/images/Image testpage_*.xml"):
+            os.remove(filename)
 
     @staticmethod
     def test_minimal_call():
@@ -49,18 +73,18 @@ class TestImages:
     @staticmethod
     def test_stdout_minimal_call():
         expected = '<mediawiki ' \
-                       'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
-                       'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
-                       '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
-                       '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
-                       '\r\n<contributor><username>mustermann</username>' \
-                       '</contributor>\r\n<text xml:space="preserve">\r\n' \
-                       '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;}} ' \
-                       '</text>\r\n' \
-                       '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
-                       'number of pages = 1 number of versions = 1\r\nwith ' \
-                       'contributions by [\'mustermann\']\r\nand file uploads' \
-                       ' on these pages: dict_keys([])\r\n'
+                   'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
+                   'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
+                   '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
+                   '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
+                   '\r\n<contributor><username>mustermann</username>' \
+                   '</contributor>\r\n<text xml:space="preserve">\r\n' \
+                   '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;}} ' \
+                   '</text>\r\n' \
+                   '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
+                   'number of pages = 1 number of versions = 1\r\nwith ' \
+                   'contributions by [\'mustermann\']\r\nand file uploads' \
+                   ' on these pages: dict_keys([])\r\n'
         expected_lin = expected.replace('\r\n', '\n').encode()
         expected_win = expected.encode()
         result = check_output(
@@ -73,18 +97,18 @@ class TestImages:
     @staticmethod
     def test_stdout_with_width_call():
         expected = '<mediawiki ' \
-                       'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
-                       'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
-                       '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
-                       '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
-                       '\r\n<contributor><username>mustermann</username>' \
-                       '</contributor>\r\n<text xml:space="preserve">\r\n' \
-                       '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;100px#124;}} ' \
+                   'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
+                   'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
+                   '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
+                   '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
+                   '\r\n<contributor><username>mustermann</username>' \
+                   '</contributor>\r\n<text xml:space="preserve">\r\n' \
+                   '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;100px#124;}} ' \
                    '</text>\r\n' \
-                       '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
-                       'number of pages = 1 number of versions = 1\r\nwith ' \
-                       'contributions by [\'mustermann\']\r\nand file uploads' \
-                       ' on these pages: dict_keys([])\r\n'
+                   '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
+                   'number of pages = 1 number of versions = 1\r\nwith ' \
+                   'contributions by [\'mustermann\']\r\nand file uploads' \
+                   ' on these pages: dict_keys([])\r\n'
         expected_lin = expected.replace('\r\n', '\n').encode()
         expected_win = expected.encode()
         result = check_output(
@@ -97,18 +121,18 @@ class TestImages:
     @staticmethod
     def test_stdout_with_width_px_call():
         expected = '<mediawiki ' \
-                       'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
-                       'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
-                       '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
-                       '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
-                       '\r\n<contributor><username>mustermann</username>' \
-                       '</contributor>\r\n<text xml:space="preserve">\r\n' \
-                       '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;600px#124;}} ' \
+                   'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
+                   'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
+                   '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
+                   '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
+                   '\r\n<contributor><username>mustermann</username>' \
+                   '</contributor>\r\n<text xml:space="preserve">\r\n' \
+                   '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;600px#124;}} ' \
                    '</text>\r\n' \
-                       '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
-                       'number of pages = 1 number of versions = 1\r\nwith ' \
-                       'contributions by [\'mustermann\']\r\nand file uploads' \
-                       ' on these pages: dict_keys([])\r\n'
+                   '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
+                   'number of pages = 1 number of versions = 1\r\nwith ' \
+                   'contributions by [\'mustermann\']\r\nand file uploads' \
+                   ' on these pages: dict_keys([])\r\n'
         expected_lin = expected.replace('\r\n', '\n').encode()
         expected_win = expected.encode()
         result = check_output(
@@ -121,19 +145,19 @@ class TestImages:
     @staticmethod
     def test_stdout_with_percentage_call():
         expected = '<mediawiki ' \
-                       'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
-                       'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
-                       '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
-                       '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
-                       '\r\n<contributor><username>mustermann</username>' \
-                       '</contributor>\r\n<text xml:space="preserve">\r\n' \
-                       '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;upright ' \
+                   'xml:lang="en">\r\n<siteinfo>\r\n<base>https://fb1-7.' \
+                   'bs.ptb.de/tiki/</base>\r\n</siteinfo>\r\n<page>\r\n' \
+                   '<title>Image testpage</title>\r\n<revision>\r\n<id>1' \
+                   '</id>\r\n<timestamp>2018-03-12T12:38:31Z</timestamp>' \
+                   '\r\n<contributor><username>mustermann</username>' \
+                   '</contributor>\r\n<text xml:space="preserve">\r\n' \
+                   '__TOC__\r\n\r\n{{File:Xwiki-logo.png#124;upright ' \
                    '1.0#124;}} ' \
                    '</text>\r\n' \
-                       '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
-                       'number of pages = 1 number of versions = 1\r\nwith ' \
-                       'contributions by [\'mustermann\']\r\nand file uploads' \
-                       ' on these pages: dict_keys([])\r\n'
+                   '</revision>\r\n</page>\r\n</mediawiki>\r\n\r\n' \
+                   'number of pages = 1 number of versions = 1\r\nwith ' \
+                   'contributions by [\'mustermann\']\r\nand file uploads' \
+                   ' on these pages: dict_keys([])\r\n'
         expected_lin = expected.replace('\r\n', '\n').encode()
         expected_win = expected.encode()
         result = check_output(
@@ -142,3 +166,12 @@ class TestImages:
              "https://fb1-7.bs.ptb.de/tiki/",
              "./test/images/Image testpage_width_percentage.tar"])
         assert (result == expected_lin or result == expected_win)
+
+    # noinspection PyPep8Naming
+    @classmethod
+    def tearDownClass(cls):
+        from glob import glob
+
+        for filename in glob("images_*.xml"):
+            print(filename)
+            # (filename)
